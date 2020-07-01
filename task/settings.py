df0852a2655da25ca567672463b6530b97d93f98
Udhaya Prasath <42908581+Udhayaprasath01@users.noreply.github.com>
2020-07-01 22:40:59 +0530

    Update settings.py

--- task/settings.py
+++ task/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@47vzgovfov6w7#e4^0w)++5lnb+c$#ss3@q6ygp74l63@#e^r'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bankDB',
         'USER': 'postgres',
-        'PASSWORD': 'udhaya',
+        'PASSWORD': 'PASSWORD',
         'HOST': 'localhost',
         'PORT': '',
     }
